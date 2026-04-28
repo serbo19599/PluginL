bfb10fda3f28f5bb3b6e55099e67216357168bdc
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-04-28 22:08:50 +0500

    Refactor plugin for custom site integration

--- plugin.js
+++ plugin.js
@@ -1,51 +1,35 @@
-// Последнее обновление: 28.04.2026
-// Версия: 1.0.5-tizen-fix
 (function () {
     'use strict';
 
-    // Вспомогательная функция (как в вашем рабочем плагине)
-    function checkCompatibility() {
-        return typeof Lampa !== 'undefined';
-    }
-
-    function MyNewPlugin() {
+    function MyCustomSite() {
         this.init = function () {
-            var ready = function () {
-                var item = {
-                    title: 'МОЙ САЙТ',
-                    icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" fill="white"/></svg>',
-                    onSelect: function () {
-                        // Прямой метод открытия для Tizen
-                        Lampa.Platform.openURL('https://www.chaturbate.best/couple-cams/');
-                    }
-                };
-                
-                // Проверка на дубликаты
-                var menu = $('.menu__list');
-                if (menu.length > 0 && !menu.data('my-site-added')) {
-                    Lampa.Menu.add(item);
-                    menu.data('my-site-added', true);
-                }
-            };
+            // Ждем полной готовности Лампы, как это делают современные моды
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type == 'ready') {
+                    var item = {
+                        title: 'МОЙ САЙТ',
+                        icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" fill="white"/></svg>',
+                        onSelect: function () {
+                            // Метод открытия, который точно работает на Tizen
+                            Lampa.Platform.openURL('https://www.chaturbate.best/couple-cams/');
+                        }
+                    };
 
-            // Ждем готовности приложения
-            if (window.appready) ready();
-            else {
-                Lampa.Listener.follow('app', function (e) {
-                    if (e.type == 'ready') ready();
-                });
-            }
+                    // Добавляем кнопку только если её еще нет
+                    var menu = $('.menu__list');
+                    if (menu.length > 0 && !window.my_site_button_added) {
+                        Lampa.Menu.add(item);
+                        window.my_site_button_added = true;
+                    }
+                }
+            });
         };
     }
 
-    // Регистрация с проверкой, чтобы не было ошибки "Не удалось загрузить"
-    try {
-        if (!window.my_site_initialized) {
-            window.my_site_initialized = true;
-            var plugin = new MyNewPlugin();
-            Lampa.Plugins.add('my_custom_site_fixed', plugin);
-        }
-    } catch (e) {
-        console.log('Plugin error:', e);
+    // Регистрация по стандартам новых плагинов
+    if (!window.my_site_plugin_instance) {
+        window.my_site_plugin_instance = true;
+        var plugin = new MyCustomSite();
+        Lampa.Plugins.add('my_custom_site_button', plugin);
     }
 })();
