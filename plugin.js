70cf7122c410fa98e0cdee4435882c68758e2e80
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-04-28 18:10:19 +0500

    Refactor plugin initialization and add menu item

--- plugin.js
+++ plugin.js
@@ -1,24 +1,18 @@
 (function () {
     'use strict';
-
-    if (window.my_final_plugin_loaded) return;
-    window.my_final_plugin_loaded = true;
-
-    function init() {
-        if (window.Lampa && Lampa.Menu) {
-            Lampa.Menu.add({
+    Lampa.Plugins.add('my_final_button', function (object) {
+        this.init = function () {
+            // Это окно покажет нам, что плагин ЗАГРУЗИЛСЯ
+            Lampa.Noty.show('Плагин успешно запущен!'); 
+            
+            var item = {
                 title: 'Мой Сайт',
                 icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" fill="white"/></svg>',
                 onSelect: function () {
-                    // Замените на нужную вам ссылку
                     Lampa.Platform.openURL('https://www.chaturbate.best/couple-cams/');
                 }
-            });
-        } else {
-            setTimeout(init, 500);
-        }
-    }
-
-    init();
+            };
+            Lampa.Menu.add(item);
+        };
+    });
 })();
- 
