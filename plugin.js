61b3de08b16a1c4ff5f5a093823deacedf7138e5
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-04-28 21:53:21 +0500

    Add custom site plugin with initialization logic
    Refactor plugin to add custom site with listener for app readiness.

--- plugin.js
+++ plugin.js
@@ -1,1 +1,26 @@
-Lampa.Noty.show('Скрипт пробился!'); Lampa.Plugins.add('my_test', function(){this.init=function(){Lampa.Menu.add({title:'МОЙ САЙТ',icon:'',onSelect:function(){Lampa.Platform.openURL('https://www.chaturbate.best/couple-cams/')}})}});
+//28.04.2026 - Update
+(function () {
+    'use strict';
+
+    function myPlug() {
+        this.init = function () {
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type == 'ready') {
+                    var item = {
+                        title: 'МОЙ САЙТ',
+                        icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" fill="white"/></svg>',
+                        onSelect: function () {
+                            Lampa.Platform.openURL('https://www.chaturbate.best/couple-cams/');
+                        }
+                    };
+                    Lampa.Menu.add(item);
+                }
+            });
+        };
+    }
+
+    if (!window.my_custom_site_loaded) {
+        window.my_custom_site_loaded = true;
+        Lampa.Plugins.add('my_custom_site', myPlug);
+    }
+})();
