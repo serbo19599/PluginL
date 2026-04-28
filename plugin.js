19b157631105bd88a14592eced02de1554044a0a
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-04-28 22:40:34 +0500

    Update URL in button action for plugin.js

--- plugin.js
+++ plugin.js
@@ -7,7 +7,7 @@
             title: 'МОЙ САЙТ',
             icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" fill="white"/></svg>',
             onSelect: function () {
-                Lampa.Platform.openURL('ВАША_ССЫЛКА');
+                Lampa.Platform.openURL('https://www.chaturbate.best/couple-cams/');
             }
         };
 
