8200e90a3d8e0c2c92f750755eaef6a4ed3b580a
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-04-29 18:24:08 +0500

    Refactor plugin.js to improve site integration

--- plugin.js
+++ plugin.js
@@ -4,64 +4,44 @@
     if (window.my_mod_active) return;
     window.my_mod_active = true;
 
-    // 1. Регистрируем сайт как "Родной раздел" Лампы
-    Lampa.Component.add('my_site_native', function (object) {
-        var comp = this;
-        var html = $('<div class="video-full"></div>');
-        var frame = $('<iframe src="' + object.url + '" style="width: 100%; height: 100%; border: none;"></iframe>');
-
-        this.create = function () {
-            html.append(frame);
-            
-            // Ждем загрузки и заставляем Лампу "увидеть" содержимое
-            frame.on('load', function() {
-                Lampa.Controller.enable('my_site_native');
-            });
-        };
-
-        this.active = function () {
-            // Передаем управление пультом этому компоненту
-            Lampa.Controller.add('my_site_native', {
-                toggle: function () {},
-                up: function () { frame[0].contentWindow.postMessage('up', '*'); },
-                down: function () { frame[0].contentWindow.postMessage('down', '*'); },
-                left: function () { frame[0].contentWindow.postMessage('left', '*'); },
-                right: function () { frame[0].contentWindow.postMessage('right', '*'); },
-                back: function () { Lampa.Activity.backward(); }
-            });
-            Lampa.Controller.toggle('my_site_native');
-        };
-
-        this.render = function () { return html; };
-        this.pause = function () {};
-        this.stop = function () {};
-        this.destroy = function () { html.remove(); };
-    });
-
     function startMod() {
         var target = $('.menu__item[data-action="tv"]');
+        
         if (target.length > 0 && !target.data('modded')) {
             target.find('.menu__text').text('МОЙ САЙТ');
             target.css('color', '#ffeb3b');
 
-            target.on('click', function (e) {
+            target.on('hover:enter click', function (e) {
                 e.preventDefault();
                 e.stopImmediatePropagation();
+                
+                var url = 'https://www.russianfood.com/recipes/recipe.php?rid=119475'; // ВАШ АДРЕС
 
-                // 2. Запускаем сайт внутри "родного" контейнера
-                Lampa.Activity.push({
-                    url: 'https://www.russianfood.com/recipes/recipe.php?rid=119475', // ВАШ САЙТ
-                    title: 'МОЙ САЙТ',
-                    component: 'my_site_native',
-                    page: 1
-                });
+                if (window.Lampa && Lampa.Platform) {
+                    // 1. Открываем сайт проверенным способом
+                    Lampa.Platform.openURL(url);
+
+                    // 2. ХИТРОСТЬ: Через 2 секунды после открытия сайта
+                    // мы заставляем Лампу "забыть" про пульт,
+                    // чтобы Tizen сам переключился в режим навигации по ссылкам.
+                    setTimeout(function() {
+                        if (Lampa.Controller) {
+                            Lampa.Controller.enabled().pause();
+                        }
+                    }, 2000);
+                }
+                
                 return false;
             });
+
             target.data('modded', true);
         }
     }
 
+    // Тот самый рабочий таймер
     setInterval(function() {
-        if (typeof $ !== 'undefined') startMod();
+        if (typeof $ !== 'undefined' && $('.menu__item[data-action="tv"]').length) {
+            startMod();
+        }
     }, 1000);
 })();
