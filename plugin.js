68d2b456b3eaf1cb8e7eaf4e318cf605194ca56e
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-04-29 18:30:10 +0500

    Change mod activation interval to 1 second
    Reduced interval check from 2 seconds to 1 second for mod activation.

--- plugin.js
+++ plugin.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
 
-    // Защита от повторного запуска
     if (window.my_mod_active) return;
     window.my_mod_active = true;
 
@@ -9,23 +8,15 @@
         var target = $('.menu__item[data-action="tv"]');
         
         if (target.length > 0 && !target.data('modded')) {
-            // 1. Меняем только текст и цвет
             target.find('.menu__text').text('МОЙ САЙТ');
             target.css('color', '#ffeb3b');
 
-            // 2. Вешаем только клик, как в самой первой рабочей версии
             target.on('click', function (e) {
                 e.preventDefault();
                 e.stopImmediatePropagation();
                 
-                var url = 'https://www.russianfood.com/recipes/recipe.php?rid=119475'; // ВАША ССЫЛКА
-
-                // Самая стабильная команда для Tizen
-                try {
-                    Lampa.Platform.openURL(url);
-                } catch(err) {
-                    window.location.href = url;
-                }
+                // Используем самую простую команду без проверок
+                Lampa.Platform.openURL('https://www.russianfood.com/recipes/recipe.php?rid=119475'); 
                 
                 return false;
             });
@@ -34,6 +25,6 @@
         }
     }
 
-    // Проверка раз в 2 секунды, чтобы не перегружать память
-    setInterval(startMod, 2000);
+    setInterval(startMod, 1000);
 })();
+
