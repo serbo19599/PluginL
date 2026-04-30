259631937df739eee6f96ab77838cb4588c12ed3
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-04-30 19:07:02 +0500

    Add custom site link to the menu

--- plugin.js
+++ plugin.js
@@ -3,14 +3,20 @@
 
     Lampa.Listener.follow('app', function (e) {
         if (e.type === 'ready') {
-            // Создаем кнопку с названием "Мой сайт"
             var item = $('<div class="menu__item selector focus">\n' +
                 '<div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" fill="white"/></svg></div>\n' +
                 '<div class="menu__text">Мой сайт</div>\n' +
                 '</div>');
 
-            // При нажатии открывается именно ваша ссылка на russianfood
             item.on('hover:enter', function () {
+                Lampa.Component.add('my_custom_site', {
+                    title: 'Мой сайт',
+                    url: 'https://www.russianfood.com/recipes/recipe.php?rid=119475',
+                    onBack: function() {
+                        Lampa.Controller.toggle('menu');
+                    }
+                });
+
                 Lampa.Activity.push({
                     url: 'https://www.russianfood.com/recipes/recipe.php?rid=119475',
                     title: 'Мой сайт',
@@ -19,7 +25,6 @@
                 });
             });
 
-            // Добавляем в боковое меню
             $('.menu .menu__list').append(item);
         }
     });
